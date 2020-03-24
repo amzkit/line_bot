7502e48c146d52bb9592c8f46823ae89f2232fb8
Amzkit <amzkit@gmail.com>
2020-03-24 23:53:04 +0700

    test 3

--- webhook.js
+++ webhook.js
@@ -1,51 +1,57 @@
-const express = require('express');
-const app = express();
-const bodyParser = require('body-parser');
-const morgan = require('morgan');
-const port = process.env.PORT || 4000;
+'use strict';
 
-// Import the appropriate class
-const {
-  WebhookClient
-} = require('dialogflow-fulfillment');
+const functions = require('firebase-functions');
+const {WebhookClient} = require('dialogflow-fulfillment');
+const {Card, Suggestion} = require('dialogflow-fulfillment');
 
-app.use(morgan('dev'))
-app.use(bodyParser.json())
+process.env.DEBUG = 'dialogflow:debug'; // enables lib debugging statements
 
-app.get('/', (req, res) => {
-  res.send({
-    success: true
-  });
-})
+exports.dialogflowFirebaseFulfillment = functions.https.onRequest((request, response) => {
+  const agent = new WebhookClient({ request, response });
+  console.log('Dialogflow Request headers: ' + JSON.stringify(request.headers));
+  console.log('Dialogflow Request body: ' + JSON.stringify(request.body));
 
-app.post('/webhook', (req, res) => {
-  console.log('POST: /');
-  console.log('Body: ',req.body);
-
-  //Create an instance
-  const agent = new WebhookClient({
-    request: req,
-    response: res
-  });
-
-  //Test get value of WebhookClient
-  console.log('agentVersion: ' + agent.agentVersion);
-  console.log('intent: ' + agent.intent);
-  console.log('locale: ' + agent.locale);
-  console.log('query: ', agent.query);
-  console.log('session: ', agent.session);
-
-  //Function Location
-  function location(agent) {
-    agent.add('Welcome to Thailand.');
+  function welcome (agent) {
+    agent.add(`Welcome to my agent!`);
   }
+
+  function fallback (agent) {
+    agent.add(`I didn't understand`);
+    agent.add(`I'm sorry, can you try again?`);
+  }
+
+  // // Uncomment and edit to make your own intent handler
+  // // uncomment `intentMap.set('your intent name here', yourFunctionHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function yourFunctionHandler(agent) {
+  //   agent.add(`This message is from Dialogflow's Cloud Functions for Firebase inline editor!`);
+  //   agent.add(new Card({
+  //       title: `Title: this is a card title`,
+  //       imageUrl: 'https://dialogflow.com/images/api_home_laptop.svg',
+  //       text: `This is the body text of a card.  You can even use line\n  breaks and emoji! 💁`,
+  //       buttonText: 'This is a button',
+  //       buttonUrl: 'https://docs.dialogflow.com/'
+  //     })
+  //   );
+  //   agent.add(new Suggestion(`Quick Reply`));
+  //   agent.add(new Suggestion(`Suggestion`));
+  //   agent.setContext({ name: 'weather', lifespan: 2, parameters: { city: 'Rome' }});
+  // }
+
+  // // Uncomment and edit to make your own Google Assistant intent handler
+  // // uncomment `intentMap.set('your intent name here', googleAssistantHandler);`
+  // // below to get this function to be run when a Dialogflow intent is matched
+  // function googleAssistantHandler(agent) {
+  //   let conv = agent.conv(); // Get Actions on Google library conv instance
+  //   conv.ask('Hello from the Actions on Google client library!'); // Use Actions on Google library
+  //   agent.add(conv); // Add Actions on Google library responses to your agent's response
+  // }
 
   // Run the proper function handler based on the matched Dialogflow intent name
   let intentMap = new Map();
-  intentMap.set('greeting', location);  // "Location" is once Intent Name of Dialogflow Agent
+  intentMap.set('Default Welcome Intent', welcome);
+  intentMap.set('Default Fallback Intent', fallback);
+  // intentMap.set('<INTENT_NAME_HERE>', yourFunctionHandler);
+  // intentMap.set('<INTENT_NAME_HERE>', googleAssistantHandler);
   agent.handleRequest(intentMap);
-});
-
-app.listen(port, () => {
-  console.log(`Server is running at port: ${port}`);
 });
